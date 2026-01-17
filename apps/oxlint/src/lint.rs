5a3a853c43649f6225e1c5ee40437dc5ce41e0ad
Connor Shea <connor.james.shea@gmail.com>
2026-01-16 23:01:03 -0700

    Add a snapshot test for a complex, valid config.

--- apps/oxlint/src/lint.rs
+++ apps/oxlint/src/lint.rs
@@ -1501,4 +1501,9 @@ export { redundant };
             .with_cwd("fixtures/invalid_config_type_difference".into())
             .test_and_snapshot(&[]);
     }
+
+    #[test]
+    fn test_valid_complex_config() {
+        Tester::new().with_cwd("fixtures/valid_complex_config".into()).test_and_snapshot(&[]);
+    }
 }
